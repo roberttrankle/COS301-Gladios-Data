77d76838d1ad77e17c8b2c978053f3b9bbe7771c
Gregory Austin <u14039712@tuks.co.za>
2017-03-22 14:55:57 +0200

    Add files via upload

---
+++ GeoLocation.java
@@ -0,0 +1,32 @@
+
+public class GeoLocation {
+
+    private double lat;
+
+    private double lon;
+
+    public GeoLocation(){
+
+    }
+
+    public GeoLocation(double lat, double lon) {
+        this.lat = lat;
+        this.lon = lon;
+    }
+
+    public void setLat(double lat) {
+        this.lat = lat;
+    }
+
+    public void setLon(double lon) {
+        this.lon = lon;
+    }
+
+    public double getLat() {
+        return lat;
+    }
+
+    public double getLon() {
+        return lon;
+    }
+}
