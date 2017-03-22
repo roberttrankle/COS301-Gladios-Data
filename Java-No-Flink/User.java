269b0d645291229d1e9b68b53884088fda6e209d
Dedrè Olwage <dedreolwage@gmail.com>
2017-03-22 23:14:08 +0200

    Update User.java
    Add JavaDoc documentation.

--- Java-No-Flink/User.java
+++ Java-No-Flink/User.java
@@ -1,4 +1,11 @@
-
+/**
+* <h1>User</h1>
+* The User class simply requests the location from the DataStream.
+*
+* @author  Dedrè Olwage
+* @version 1.0
+* @since   3/22/2017
+*/
 import java.util.Random;
 
 
@@ -8,6 +15,11 @@ public class User extends Thread
 	public UserRequest user = null;
 	DataStream Manager = null;
 
+
+	/**
+	 *This is the constructor method initializing the Mac Address of the user.
+	 *@param mac This parameter contains the Mac address.
+	 */
 	public User(String mac,DataStream manager)
 	{
 		user = new UserRequest(mac);
@@ -15,6 +27,13 @@ public class User extends Thread
 		start();
 	}
 
+	/**
+	 *This method continuosly request the user's location.'
+	 *Random boolean values are chosen to determine whether the\
+	 *user wants to request the location or not.
+	 *In the real implementation, the user might
+	 *request their location unlimitedly.
+	 */
 	 public void run() {
 
 		Random random = new Random();
@@ -34,6 +53,11 @@ public class User extends Thread
 		}
 	 }
 
+	 /**
+	 *This method will set request the user's current location.
+	 *The user adds itsel to a queue, and waits until the
+	 *DataStream can handle its request.
+	 */
 	public void requestLocation()
 	{
 		Manager.addUser(user);
