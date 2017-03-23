8d0609df201e7a456cfadd2da5eeb645ce1620ab
Dedrè Olwage <dedreolwage@gmail.com>
2017-03-23 06:40:52 +0200

    Update Server.java
    Set userMac variable to the Mac Address sent in as parameter to the getLocation function.

--- Java-No-Flink/Server.java
+++ Java-No-Flink/Server.java
@@ -2,19 +2,14 @@
 * <h1>Server</h1>
 * The Server class simply handles requests from the DataStream class.
 *
-* @author  Dedrè Olwage
+* @author  Dedrè Olwage, Oratile Motswagosele
 * @version 1.0
 * @since   3/22/2017
 */
 public class Server
 {
 	private String  userMac;
-	
-	public Server()
-	{
-		userMac = new MacAddress().getMacAddress();
-	}
-	
+
 	/**
 	 *This method will request the users location from the GeoLocation class.
 	 *@param mac The user's Mac Address that will be sent to the GeoLocation class (for future implementaion purposes.)
@@ -22,6 +17,7 @@ public class Server
 	 */
 	public String getLocation(String mac)
 	{
+			userMac = mac;
 			GeoLocation location = new GeoLocation(mac);
 
 			String coordinates = location.getGeoLocation();
