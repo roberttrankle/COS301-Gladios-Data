53ddc0128f0fb6d677a4c3e39db4a09e4cd8bcab
Dedrè Olwage <dedreolwage@gmail.com>
2017-03-23 21:48:26 +0200

    Update DataInterface.java
    Fix minor errors.

--- Java-No-Flink/DataInterface.java
+++ Java-No-Flink/DataInterface.java
@@ -1,9 +1,10 @@
-public interface DataInterface{
+public class DataInterface{
 
 	public GeoLocation getLocation( String macAddress ){
 
+		
 		GeoLocation geo = new GeoLocation( macAddress );
-		if( geo.getLat() == null || geo.getLon() == null || geo.getfloor() == null )
+		if( geo.getLat() == 0 || geo.getLon() == 0 || geo.getFloor() == 0 )
 			return null ;
 		return geo ;
 	}
