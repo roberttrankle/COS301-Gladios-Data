a6dfb2e12a8dc45e0c331bfa5ef408892b8e9e98
Kenneth <k.mangwane@pronerd.co.za>
2017-03-24 12:39:32 +0200

    Update DataInterface.java

--- Java-No-Flink/DataInterface.java
+++ Java-No-Flink/DataInterface.java
@@ -1,3 +1,14 @@
+/**
+* <h1>UserRequest</h1>
+* The UserRequest class simply stores,sets and returns basic information about the user
+*such as a Mac Address and a boolean value indicating whether the user wants to 
+*get their current location or not.
+*
+* @author  Tlou Lebelo, Kenneth Mangwane
+* @version 1.0
+* @since   3/23/2017
+*/
+
 public class DataInterface{
 
 	public GeoLocation getLocation( String macAddress ){
